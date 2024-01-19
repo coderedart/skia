2090f1fe5b766dad2e0b7482b4f39ebd5155bba6
Red Artist <redartofcode@gmail.com>
2024-01-19 20:04:02 +0530

    create workflow to build on as many targets as possible

--- ckia/build_skia.sh
+++ ckia/build_skia.sh
@@ -8,8 +8,19 @@ GIT_SYNC_DEPS_SHALLOW_CLONE=True GIT_SYNC_DEPS_SKIP_EMSDK=True python tools/git-
 echo "removing useless sources"
 python ckia/rm_useless_src.py
 
+GN_ARGS="$IS_OFFICIAL_BUILD $IS_COMPONENT_BUILD $USE_VULKAN $FULL_ARGS $TARGET_ARGS"
+echo "$GN_ARGS"
+if [[ $TARGET_OS_ARG != "" ]]; then
+    GN_ARGS="target_os=\"$TARGET_OS_ARG\" $GN_ARGS"
+fi
+if [[ $TARGET_OS_ARG == "android" ]]; then
+    GN_ARGS="ndk=\"$ANDROID_NDK_ROOT\" $GN_ARGS"
+fi
+GN_ARGS="target_cpu=\"$TARGET_ARCH_ARG\" $GN_ARGS"
+
+
 echo "building skia libs"
-./bin/gn gen out --args="$IS_OFFICIAL_BUILD $IS_COMPONENT_BUILD $USE_VULKAN $FULL_ARGS"
+./bin/gn gen out --args="$GN_ARGS"
 ./bin/gn args --list --short out
 ./bin/gn desc out :skia
 ninja -C out
@@ -24,12 +35,12 @@ if [[ "$IS_OFFICIAL_BUILD" == "is_official_build=true" ]]; then
         echo "zip name is $ZIP_NAME"
         if [[ "$TARGET" =~ "windows" ]]; then
             tar -czvf "$ZIP_NAME" ./*.dll icudtl.dat
-        elif [[ "$TARGET" =~ "darwin" ]]; then
+        elif [[ "$TARGET" =~ "darwin" || "$TARGET" == "ios" ]]; then
             tar -czvf "$ZIP_NAME" ./*.dylib
-        elif [[ "$TARGET" == "linux" ]]; then
+        elif [[ "$TARGET" == "linux" || "$TARGET" == "android" ]]; then
             tar -czvf "$ZIP_NAME" ./*.so
         else
-            echo "unknown platform"
+            echo "unknown platform $TARGET"
             tar -czvf "$ZIP_NAME" ./*.so
         fi
     else
@@ -37,12 +48,10 @@ if [[ "$IS_OFFICIAL_BUILD" == "is_official_build=true" ]]; then
         echo "zip name is $ZIP_NAME"
         if [[ "$TARGET" =~ "windows" ]]; then
             tar -czvf "$ZIP_NAME" ./*.lib icudtl.dat
-        elif [[ "$TARGET" =~ "darwin" ]]; then
-            tar -czvf "$ZIP_NAME" ./*.a
-        elif [[ "$TARGET" == "linux" ]]; then
+        elif [[ "$TARGET" =~ "darwin" || "$TARGET" == "linux" || "$TARGET" == "android" || "$TARGET" == "ios" ]]; then
             tar -czvf "$ZIP_NAME" ./*.a
         else
-            echo "unknown platform"
+            echo "unknown platform $TARGET"
             tar -czvf "$ZIP_NAME" ./*.a
         fi
     fi
